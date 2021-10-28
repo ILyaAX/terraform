56a834f62be504d1eaf253323dab22a22476ce5f
Ilya Aksenov <ilya@aksenov.ws>
2021-10-28 19:43:29 +0700

    synt

--- config.tf
+++ config.tf
@@ -33,24 +33,20 @@ resource "aws_security_group" "nginx" {
   name        = "nginx"
   description = "http"
 
-  ingress = [
-    {
+  ingress {
       description      = "http"
       from_port        = 80
       to_port          = 80
       protocol         = "tcp"
       cidr_blocks      = ["0.0.0.0/0"]
     }
-  ]
 
-  egress = [
-    {
+  egress {
       from_port        = 0
       to_port          = 0
       protocol         = "-1"
       cidr_blocks      = ["0.0.0.0/0"]
     }
-  ]
 
   tags = {
     Name = "nginx"
